6c9361de360b03e4b4894ccab64d30fd83caff8c
alvaroarmijo90-droid <alvaroarmijo90@gmail.com>
2026-02-19 18:50:35 -0400

    config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 // config.js - CONFIGURACIÓN DE FIREBASE
 const CONFIG_FIREBASE = {
-    apiKey: "AIzaSyCexmO-DZrMmcPImII4ff5FYDN8YV0O09Y",
+    apiKey: "AIzaSyBLAh9lHjKJgdfgkjh65fd4g5d4f5g6d7f8g9",
     authDomain: "federacion-estudiantil.firebaseapp.com",
     projectId: "federacion-estudiantil",
-    storageBucket: "federacion-estudiantil.firebasestorage.app",
-    messagingSenderId: "776623574249",
-    appId: "1:776623574249:web:bd1ef98b867a8928987478"
+    storageBucket: "federacion-estudiantil.appspot.com",
+    messagingSenderId: "123456789012",
+    appId: "1:123456789012:web:abc123def456ghi789"
 };
-
 
 const CONFIG_APP = {
     ADMIN_PASSWORD: "admin123",
@@ -15,5 +14,4 @@ const CONFIG_APP = {
     NOMBRE_FEDERACION: "Federación Estudiantil"
 };
 
-console.log('⚙️ Config.js cargado');
-
+console.log('⚙️ Config.js para Firebase cargado');
